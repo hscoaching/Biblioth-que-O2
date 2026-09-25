deaa8dc6661a7236afcd8f9636291167a934d76d
hscoaching <hsport78100@gmail.com>
2026-09-26 01:46:39 +0200

    chore: add centralized features config

---
+++ features.js
@@ -0,0 +1,3 @@
+// Configuration centralisée des fonctionnalités HS Coaching.
+// Les modules peuvent ajouter leurs propres options sans multiplier les fichiers de configuration.
+window.HS_FEATURES = window.HS_FEATURES || {};
